4e9fb29a0bb668b1b8083beced1e30246bbd1b6f
Manuel Ernesto <manuelernesto@MacBook-Pro-de-Manuel.local>
2018-07-08 16:42:09 +0100

    Simple ListView

--- app/src/main/java/io/github/manuelernesto/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/io/github/manuelernesto/coderswag/Controller/MainActivity.kt
@@ -2,12 +2,21 @@ package io.github.manuelernesto.coderswag.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import io.github.manuelernesto.coderswag.Model.Category
 import io.github.manuelernesto.coderswag.R
+import io.github.manuelernesto.coderswag.Services.DataService
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter: ArrayAdapter<Category>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, DataService.categories)
+        categoryListView.adapter = adapter
     }
 }
--- app/src/main/java/io/github/manuelernesto/coderswag/Model/Category.kt
+++ app/src/main/java/io/github/manuelernesto/coderswag/Model/Category.kt
@@ -1,3 +1,7 @@
 package io.github.manuelernesto.coderswag.Model
 
-class Category constructor(val title: String, val image: String)
+class Category constructor(val title: String, val image: String){
+    override fun toString(): String {
+        return this.title
+    }
+}
